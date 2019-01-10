d579f74034c8dbe237d3e856701e4a305e785976
Jinhui Zhu <jinhuizhu@gmail.com>
2019-01-10 23:02:52 +0800

    add sample bottom/checkbox

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {
   MatButtonModule,
-  MatCheckboxModule} from "@angular/material";
+  MatCheckboxModule
+} from "@angular/material";
 
 /**
  * Angular Material modules
  */
 @NgModule({
   imports: [
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule
+  ],
+  exports: [
     BrowserAnimationsModule,
     MatButtonModule,
     MatCheckboxModule
