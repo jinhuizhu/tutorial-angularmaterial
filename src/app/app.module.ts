b25f519ac096cf17e0c14c4f0acd87861a594696
Jinhui Zhu <jinhuizhu@gmail.com>
2019-01-10 23:25:36 +0800

    import all material modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {MaterialModule} from "./shared/material.module";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import {MaterialModule} from "./shared/material.module";
   ],
   imports: [
     BrowserModule,
-    MaterialModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
