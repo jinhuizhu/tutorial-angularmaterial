d139164377b07c767ccccb565761b168d792bee5
Jinhui Zhu <jinhuizhu@gmail.com>
2019-01-13 23:26:23 +0800

    add toolbar, make sidenav look better, add main content

--- src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+const SMALL_WIDTH_BREAKPOINT = 720;
+
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavComponent implements OnInit {
 
+  private mediaMatcher: MediaQueryList = matchMedia(`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`);
+
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  isScreenSmall(): boolean {
+    return this.mediaMatcher.matches;
   }
 
 }
