b50d30032ea5a093b234ffe6aefd59fbbe36bb42
Jinhui Zhu <jinhuizhu@gmail.com>
2019-01-26 21:00:26 +0800

    add paginator example

--- src/app/contactmanager/components/notes/notes.component.ts
+++ src/app/contactmanager/components/notes/notes.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {Note} from "../../models/note";
+import {MatPaginator, MatTableDataSource} from "@angular/material";
 
 @Component({
   selector: 'app-notes',
@@ -9,10 +10,15 @@ import {Note} from "../../models/note";
 export class NotesComponent implements OnInit {
 
   @Input() notes: Note[];
+  displayedColumns: string[] = ['position', 'title', 'date'];
+  dataSource: MatTableDataSource<Note>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor() { }
 
   ngOnInit() {
-  }
+    this.dataSource = new MatTableDataSource<Note>(this.notes);
+    this.dataSource.paginator = this.paginator;
 
+  }
 }
